6abf77822fff81593c7554f930cd6edd072d64f4
Federico Burch <federicoburch@gmail.com>
2014-02-24 20:08:03 -0500

    Multiverse is an application with multiple activities, views and simple buttons and checkboxes.
    This program is a test to understand how to build different
    activites/views and link them together via Intents.

---
+++ MultiVerse/gen/com/discoverfed/multiverse/BuildConfig.java
@@ -0,0 +1,6 @@
+/** Automatically generated file. DO NOT MODIFY */
+package com.discoverfed.multiverse;
+
+public final class BuildConfig {
+    public final static boolean DEBUG = true;
+}
---
+++ MultiVerse/src/com/discoverfed/multiverse/OtherActivity.java
@@ -0,0 +1,15 @@
+package com.discoverfed.multiverse;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class OtherActivity extends Activity {
+	
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.other_activity);
+
+	}
+
+}
